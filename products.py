f19cd2da18a03ecc0df4d73ec04916730b4d45e4
Linhanrr <hank093011@gmail.com>
2021-07-13 23:38:15 +0800

    add check file exists

--- products.py
+++ products.py
@@ -1,11 +1,20 @@
-#從舊檔案讀取
-products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f: #編碼問題在讀取跟寫入時都會出現，所以要統一
-    for line in f:
-        name, price = line.strip().split(',') #每一行去掉換行符號後，用split在,處分隔，並分別存入name price
-        products.append([name, price])
-print(products)
+import os # operting system 
 
+#讀取檔案
+products = []
+if os.path.isfile('products.csv'): #檢查檔案在不在，運用os模組裡的path中isfile涵式的功能
+    print('yeah 找到檔案了')
+    with open('products.csv', 'r', encoding = 'utf-8') as f: #編碼問題在讀取跟寫入時都會出現，所以要統一
+        for line in f:
+            if '商品, 價格' in line:
+                continue #跳到下一個迴圈
+            name, price = line.strip().split(',') #每一行去掉換行符號後，用split在,處分隔，並分別存入name price
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案qq')
+
+#讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -14,9 +23,11 @@ while True:
     price = int(price)
     products.append([name, price])
 
+#印出所有購買紀錄
 for p in products:
     print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
     f.write('商品, 價格\n')
     for p in products:
